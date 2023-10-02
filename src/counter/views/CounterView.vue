8a10cdca273f091a0c845dd9ae993432cc4d5f0d
Willian <williamhernandez450@gmail.com>
2023-10-02 07:13:58 -0600

    Using composition API for counter

--- src/counter/views/CounterView.vue
+++ src/counter/views/CounterView.vue
@@ -1,9 +1,33 @@
 <template>
-    <h1>Counter View: 10</h1>
-    <button>+1</button>
+    <h1>Counter View: {{ counter }}</h1>
+    <button @click="increase">+1</button>
 </template>
 
 <script lang="ts" setup>
+import { ref } from 'vue';
+
+// composition API without setup
+// export default defineComponent({
+//     setup() {
+//         const counter = ref(5);
+
+//         const increase = () => {
+//             counter.value++;
+//         }
+
+//         return {
+//             counter,
+//             increase
+//         }
+//     }
+// });
+
+// composition API with septup
+const counter = ref(5);
+const increase = () => {
+    counter.value++
+}
+
 </script>
 
 <style scoped>
