37fcd71c050df7b735b223ba637a96ca4a274fef
Willian <williamhernandez450@gmail.com>
2023-11-12 17:26:49 -0600

    Catching errors and refactor code in component

--- src/clients/composables/useClient.ts
+++ src/clients/composables/useClient.ts
@@ -1,20 +1,31 @@
 import clientsApi from "@/data/api/clientsApi";
 import type { Client } from "@/data/intefaces/clients/Client"
-import { useQuery } from "@tanstack/vue-query";
-import { ref, watch } from "vue";
+import { useMutation, useQuery } from "@tanstack/vue-query";
+import { computed, ref, watch } from "vue";
 
 const getClient = async(id: number):Promise<Client> => {
     const { data } = await clientsApi.get(`/clients/${id}`);
     return data;
 }
 
+const updateClient = async( client: Client ):Promise<Client> => {
+
+    const { data } = await clientsApi.patch<Client>(`/clients/${ client.id }`, client);
+
+    return data;
+}
+
 const useClient = (id: number) => {
 
     const client = ref<Client>();
+    const clientMutation = useMutation( updateClient );
 
-    const { isLoading, data } = useQuery(
+    const { isLoading, data, isError } = useQuery(
         ['client', id],
-        () => getClient(id)
+        () => getClient(id),
+        {
+            retry: false
+        }
     );
 
     watch( data, () => {
@@ -23,7 +34,13 @@ const useClient = (id: number) => {
 
     return {
         isLoading,
-        client
+        clientMutation,
+        client,
+        isError,
+        updateClient:                   clientMutation.mutate,
+        isUpdating: computed(() =>      clientMutation.isLoading.value),
+        isUpdatingSuccessfull: computed(() => clientMutation.isSuccess.value),
+        isErrorUpdating: computed(() => clientMutation.isError.value)
     }
 }
 
--- src/clients/views/ClientView.vue
+++ src/clients/views/ClientView.vue
@@ -1,13 +1,13 @@
 <template>
 <div>
-    <h3 v-if="clientMutation.isLoading.value">Guardando...</h3>
-    <h3 v-if="clientMutation.isSuccess.value">Guardado</h3>
+    <h3 v-if="isUpdating">Guardando...</h3>
+    <h3 v-if="isUpdatingSuccessfull">Guardado</h3>
 
     <LoadingModal v-if="isLoading"/>
 
     <div v-if="client">
         <h1>{{ client.name }}</h1>
-        <form @submit.prevent="clientMutation.mutate(client)">
+        <form @submit.prevent="updateClient(client)">
             <div class="row g-3">
                 <div class="col-auto mb-3">
                 <input type="text" class="form-control" 
@@ -20,7 +20,7 @@
                 </div>
 
                 <div class="col-auto mb-1">
-                    <button :disabled="clientMutation.isLoading.value" class="btn btn-primary" type="submit">Guardar</button>
+                    <button :disabled="isUpdating" class="btn btn-primary" type="submit">Guardar</button>
                 </div>
             </div>
         </form>  
@@ -36,41 +36,27 @@
 <script lang="ts" setup>
 import LoadingModal from '@/shared/components/LoadingModal.vue';
 import useClient from '../composables/useClient';
-import { useRoute } from 'vue-router';
-import { useMutation, useQueryClient } from '@tanstack/vue-query';
-import type { Client } from '@/data/intefaces/clients/Client';
-import clientsApi from '@/data/api/clientsApi';
+import { useRoute, useRouter } from 'vue-router';
+import { useQueryClient } from '@tanstack/vue-query';
 import { watch } from 'vue';
 
 const route = useRoute();
 const queryClient = useQueryClient();
+const router = useRouter();
 
 const { id } = route.params;
-const { client, isLoading } = useClient( Number(id) );
-
-const updateClient = async( client: Client ):Promise<Client> => {
-    // const { id, ...rest } = client;
-    // console.log({ client });
-
-    await new Promise( resolve => {
-        setTimeout(() => {
-            resolve(true);
-        }, 3000);
-    })
-
-    const { data } = await clientsApi.patch<Client>(`/clients/${ client.id }`, client);
-    const queries = queryClient.getQueryCache().findAll(['clients?page='], { exact: false });
-    queries.forEach( query => query.fetch() );
-
-    return data;
-}
-
-const clientMutation = useMutation( updateClient );
+const { client, isLoading, isError, clientMutation, updateClient, isUpdating, isErrorUpdating, isUpdatingSuccessfull } = useClient( Number(id) );
 
 watch( clientMutation.isSuccess, () =>  {
     setTimeout(() => {
         clientMutation.reset();
     }, 2000);
+});
+
+watch( isError, () => {
+    if ( isError.value ) {
+        router.replace('/clients')
+    }
 })
 
 </script>
